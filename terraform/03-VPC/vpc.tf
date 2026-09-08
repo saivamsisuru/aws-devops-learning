614526d0b41bdd2426cdbb9896659e73b835f8fd
saivamsisuru <78493871+saivamsisuru@users.noreply.github.com>
2026-09-08 10:42:55 +0530

    commit

--- terraform/03-VPC/vpc.tf
+++ terraform/03-VPC/vpc.tf
@@ -47,12 +47,12 @@ resource "aws_route_table" "public_rt" {
     }
 
     tags = {
-        Name - "public-route-table"
+        Name = "public-route-table"
     }
 }
 
 resource "aws_route_table_association" "public_assoc" {
     subnet_id = aws_subnet.public.id
     route_table_id = aws_route_table.public_rt.id
-    
+
 }
